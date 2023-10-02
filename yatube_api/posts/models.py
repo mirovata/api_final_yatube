4b05eb4e7eba95757ebc40090d9788122c206f7c
Роман Ткаченко <mirovata@gmail.com>
2023-10-02 23:27:42 +0500

    gf

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -67,6 +67,9 @@ class Post(models.Model):
         verbose_name='Группа'
     )
 
+    class Meta:
+        ordering = ['pub_date', ]
+
     def __str__(self):
         return self.text[:ROW_LIMIT_TO]
 
